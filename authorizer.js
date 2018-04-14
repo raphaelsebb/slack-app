7c10681bc9acfd83d91145355ac65bb0c0b4b1b3
Sebban <raph.sebban@gmail.com>
2018-04-14 17:50:18 +0200

    update authorization function

--- authorizer.js
+++ authorizer.js
@@ -1,15 +1,29 @@
 'use strict';
 
+const request = require('request-promise');
+
 module.exports = (code) => {
   console.log('Authorizer was called');
 
   const clientId = process.env.SLACK_CLIENT_ID;
   const clientSecret = process.env.SLACK_CLIENT_SECRET;
-
   const oauthURL = 'https://slack.com/api/oauth.access?'
     + 'client_id=' + client_id + '&'
     + 'client_secret=' + client_secret + '&'
     + 'code=' + code;
 
   console.log(oauthURL);
+
+  const options = {
+    url: oauthURL,
+    json: true,
+  };
+
+  return request(options)
+  .then(response => {
+    console.log(response.access_token);
+  })
+  .catch(error => {
+    console.log('error: ', error);
+  });
 };
--- handler.js
+++ handler.js
@@ -5,15 +5,29 @@ const authorizer = require('./authorizer');
 module.exports.authorization = (event, context, callback) => {
   const code = event.queryStringParameters.code;
   console.log(code);
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Authorization was called',
-      input: event,
-    }),
-  };
 
-  callback(null, response);
+  authorizer(code)
+  .then( => {
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'Authorization was called',
+        input: event,
+      }),
+    };
+    callback(null, response);
+  })
+  .catch(error => {
+    console.log('error');
+    const response = {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: error,
+        input: event,
+      }),
+    };
+    callback(null, response);
+  });
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
